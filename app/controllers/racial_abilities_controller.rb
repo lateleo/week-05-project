3a2f1cfdaecb4718e4565c3c05659b5ff78085fb
Art Burtch <artburtch@huskers.unl.edu>
2016-04-14 11:51:23 -0500

    error messages removed and racial ability controller references should be working properly now

--- app/controllers/racial_abilities_controller.rb
+++ app/controllers/racial_abilities_controller.rb
@@ -5,7 +5,7 @@ get "/races/:id/abilities/new" do
   redirect("/races/error") if !@race
   @page_name = "#{@race.name}: New Racial Ability"
   @racial_ability = RacialAbility.new
-  erb :"races/new_ability"
+  erb :"racial_abilities/new"
 end
 
 post "/races/:id/abilities/new" do
@@ -17,13 +17,13 @@ post "/races/:id/abilities/new" do
                                       in_game_effect: params['in_game_effect'],
                                       flavor_text: params['flavor_text'],
                                       race_id: params['id'])
-  @racial_ability.save ? redirect("/races/#{@race.id}") : (erb :"races/new_ability")
+  @racial_ability.save ? redirect("/races/#{@race.id}") : (erb :"racial_abilities/new")
 end
 
 get "/races/:id/abilities/error" do
   @race = Race.find_by_id(params['id'])
   @page_name = "Error: Racial Ability Not Found"
-  erb :"races/error_ability"
+  erb :"racial_abilities/error"
 end
 
 get "/races/:id/abilities/:url_name" do
@@ -31,7 +31,7 @@ get "/races/:id/abilities/:url_name" do
   @racial_ability = RacialAbility.find_by(race_id: params['id'], url_name: params['url_name'])
   !@race ? redirect("/races/error") : (redirect("/races/#{@race.id}/abilities/error") if !@racial_ability)
   @page_name = "#{@race.name}: #{@racial_ability.name}"
-  erb :"races/show_ability"
+  erb :"racial_abilities/show"
 end
 
 get "/races/:id/abilities/:url_name/edit" do
@@ -39,7 +39,7 @@ get "/races/:id/abilities/:url_name/edit" do
   @racial_ability = RacialAbility.find_by(race_id: params['id'], url_name: params['url_name'])
   !@race ? redirect("/races/error") : (redirect("/races/#{@race.id}/abilities/error") if !@racial_ability)
   @page_name = "#{@racial_ability.name} - Edit Info"
-  erb :"races/edit_ability"
+  erb :"racial_abilities/edit"
 end
 
 post "/races/:id/abilities/:url_name/edit" do
@@ -52,7 +52,7 @@ post "/races/:id/abilities/:url_name/edit" do
                                     cooldown: params['cooldown'],
                                     in_game_effect: params['in_game_effect'],
                                     flavor_text: params['flavor_text']
-    ) ? redirect("/races/#{@race.id}/abilities/#{@racial_ability.url_name}") : (erb :"races/edit_ability")
+    ) ? redirect("/races/#{@race.id}/abilities/#{@racial_ability.url_name}") : (erb :"racial_abilities/edit")
 end
 
 
